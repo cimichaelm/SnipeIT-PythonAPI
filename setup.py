ecd45fbe61a29a24ab92159f9bddf21950deab2a
cimichaelm <michaelm@corp.citenet.net>
2022-05-05 11:23:16 -0400

    update version to 1.3

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.rst","r") as fh:
 	long_description = fh.read()
 	
 setup(name='snipeit',
-      version='1.2',
+      version='1.3.1',
 	  long_description=long_description,
       long_description_content_type="text/markdown",
       description=("Python library to access the SnipeIT API"),
-      url='https://github.com/jbloomer/SnipeIT-PythonAPI',
+      url='https://github.com/cimichaelm/SnipeIT-PythonAPI',
       author='Jared Bloomer (Cox Automotive Inc.)',
       author_email='jared.bloomer@coxautoinc.com',
       license='MIT',
